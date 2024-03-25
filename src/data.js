bd4cf19a690922400fe60e55aec3210eb4fa23d6
unknown <PeterDiyo>
2024-03-25 10:37:09 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Diyo Peter",
+    job: "Software Engineer",
+    image:
+      "https://scontent-tir3-2.xx.fbcdn.net/v/t39.30808-6/428658994_1564939230927396_1185999538030011565_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=OokP-V4NcjIAX_FEBTP&_nc_oc=AdjJS8K2gfr7tpeFQ6oCEdW13Jn58rAGhta_lN4xkGen3vOarnyIwMRYyUZcsu112ow&_nc_ht=scontent-tir3-2.xx&cb_e2o_trans=q&oh=00_AfB0h5LEgGAb7gPZBXSb1HII3C-OR0p-ITy9GI4vxv54iw&oe=66041231",
+    text: "Hi, I am a Software engineer. Software Engineering is the process of designing, developing, testing, and maintaining software. It is a systematic and disciplined approach to software development that aims to create high-quality, reliable, and maintainable software systems",
+  },
+  {
+    id: 2,
+    name: "Le Boss",
+    job: "Developer",
+    image:
+      "https://scontent-tir3-2.xx.fbcdn.net/v/t39.30808-6/383237046_328801936341033_2935473463390485703_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=dYPuJqtRLTUAX_XZr04&_nc_ht=scontent-tir3-2.xx&cb_e2o_trans=q&oh=00_AfAQZaDCQ5_lHczNlcHq86AbvJ9y_JCPRkjvMb8r8ymKyA&oe=66043123",
+    text: "Software Development is defined as the process of designing, creating, testing, and maintaining computer programs and applications. The developer does software decelopment for the users interface and user experince. Godwin Le Boss, Godwin Le Boss Godwin Le Boss Godwin Le ",
+  },
+  {
+    id: 3,
+    name: "Riiny T.B",
+    job: "Optometrist",
+    image:
+      "https://scontent-tir3-2.xx.fbcdn.net/v/t39.30808-6/253727534_1667793690091228_3371258278060854055_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=N5uZsQv9mc4AX-Wih59&_nc_ht=scontent-tir3-2.xx&cb_e2o_trans=q&oh=00_AfDLv3VysDGgrFOhiD6U6iFBo2N2hmkZSyrTZXqc5zEU9w&oe=6604F7C1",
+    text: "An optometrist is a healthcare professional who provides primary vision care. He/she is licensed to practice optometry, which includes giving eye exams, writing prescriptions, and diagnosing and treating some eye conditions and diagnosing and treating some eye conditions",
+  },
+  {
+    id: 4,
+    name: "Lawrence M",
+    job: "Business analyst",
+    image:
+      "https://scontent-tir3-2.xx.fbcdn.net/v/t39.30808-6/328468559_718145763260922_3095174908306896976_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=GwvCQS2G2ycAX80ndaL&_nc_ht=scontent-tir3-2.xx&cb_e2o_trans=q&oh=00_AfAkhZIBAMCgyhEepVIySklQU3EYt7Vz55kU1gWX_PGJLg&oe=66050A6E",
+    text: "A business analyst is a person who analyzes and assesses the current business processes, products, services, and systems using data analytics tools and approaches A business analyst is a person who analyzes and assesses the current business processes, products, services",
+  },
+];
+
+export default reviews;
